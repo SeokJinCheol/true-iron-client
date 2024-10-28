5481f8558363be17ff0a2e6aa35005e0f57f8b3c
석진철 <jincheolseok@i-bricks.co.kr>
2024-10-28 20:09:25 +0900

    [add] init storybook

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import './style.scss';
+
+interface Props {}
+
+const Home: FC<Props> = () => {
+    return (
+        <div>
+            <i className="ri-user-2-fill" />
+        </div>
+    )
+}
+
+export default Home;
