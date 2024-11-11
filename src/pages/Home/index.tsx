c8c641df21bf661c08236ae28395c515c543417c
석진철 <tjrwlscjf13@gmail.com>
2024-11-11 15:26:04 +0900

    [feat] test push

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,7 @@ const Home: FC<Props> = () => {
     const user = useSelector((state: RootState) => state.user);
     const Click = () => {
         dispatch(setUser({
-            id: "SeokJinCheol",
+            id: "tjrwlscjf13",
             name: "석진철",
             email: "tjrwlscjf13@naver.com"
         }))
